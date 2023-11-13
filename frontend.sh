5aa877bdb0ac63c197ee50567d74689303048e0f
mayuran26 <bcamayu26@gmail.com>
2023-11-13 09:50:18 +0100

    changes in frontend.sh

--- frontend.sh
+++ frontend.sh
@@ -1,12 +1,16 @@
 dnf install nginx -y
 
-systemctl enable nginx
-systemctl start nginx
-cp expense.conf /etc/nginx/default.d/expense.conf
-
 rm -rf /usr/share/nginx/html/*
+
+cp expense.conf /etc/nginx/default.d/expense.conf
 
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
 
+cd /usr/share/nginx/html
+unzip /tmp/frontend.zip
+
+systemctl enable nginx
+
 systemctl restart nginx
+
 
